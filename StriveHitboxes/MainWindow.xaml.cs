e292c0e54de27bdff56cf22335a1ac3677c9e6ee
AzureTestament <kp72002@gmail.com>
2021-11-13 14:49:07 -0800

    Initial commit

---
+++ StriveHitboxes/MainWindow.xaml.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Drawing;
+using System.ComponentModel;
+
+namespace StriveHitboxes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string colPath;
+        PACFile col;
+        OverlaidImage oi;
+        List<PACFile.PACEntry> editedBoxes = new List<PACFile.PACEntry>();
+        private int oldListIndex = -1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PAC files (*.pac)|*.pac|All files (*.*)|*.*";
+            openFileDialog.Title = "Open Collision PAC";
+            if (openFileDialog.ShowDialog() == true) 
+            {
+                File.Delete(colPath + ".tmp");
+                colPath = openFileDialog.FileName;
+                col = new PACFile(openFileDialog.FileName);
+                byte[] oldpac = File.ReadAllBytes(colPath);
+                File.WriteAllBytes(colPath + ".tmp", oldpac);
+                spriteList.Items.Clear();
+                boxList.Items.Clear();
+                foreach(var entry in col.pacentries)
+                {
+                    spriteList.Items.Add(entry);
+                }
+            }
+        }
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+        private void spriteList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (spriteList.Items.Count > 0)
+            {
+                PACFile.PACEntry colentry = (PACFile.PACEntry)spriteList.Items[spriteList.SelectedIndex];
+                var coloffset = col.getOffsetByName(colentry.name);
+                if (oldListIndex > 0)
+                {
+                    var oldEntry = (PACFile.PACEntry)spriteList.Items[oldListIndex];
+                    int idx = editedBoxes.BinarySearch(oldEntry);
+                    if (idx < 0) idx = ~idx;
+                    editedBoxes.Insert(idx, oldEntry);
+                }
+                oldListIndex = spriteList.SelectedIndex;
+                oi = new OverlaidImage(col.path, coloffset);
+                boxList.Items.Clear();
+                foreach (var box in oi.hurtboxes)
+                    boxList.Items.Add(box);
+                foreach (var box in oi.hitboxes)
+                    boxList.Items.Add(box);
+                if (boxList.Items.Count != 0)
+                    boxList.SelectedIndex = 0;
+                spriteBox.Source = BitmapToImageSource(oi.boxbitmap);
+            }
+            else
+            {
+                spriteList.Items.Clear();
+                boxList.Items.Clear();
+                editedBoxes.Clear();
+            }
+        }
+
+        private void boxList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (boxList.Items.Count > 0)
+            {
+                JonbinBox currbox = (JonbinBox)boxList.Items[boxList.SelectedIndex];
+                xPos.TextChanged -= xPos_TextChanged;
+                yPos.TextChanged -= yPos_TextChanged;
+                xScl.TextChanged -= xScl_TextChanged;
+                yScl.TextChanged -= yScl_TextChanged;
+                xPos.Text = currbox.x.ToString();
+                yPos.Text = currbox.y.ToString();
+                xScl.Text = currbox.width.ToString();
+                yScl.Text = currbox.height.ToString();
+                xPos.TextChanged += xPos_TextChanged;
+                yPos.TextChanged += yPos_TextChanged;
+                xScl.TextChanged += xScl_TextChanged;
+                yScl.TextChanged += yScl_TextChanged;
+            }
+        }
+
+        private void xPos_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (boxList.Items.Count > 0)
+            {
+                JonbinBox currbox = (JonbinBox)boxList.Items[boxList.SelectedIndex];
+                float newx;
+                if (float.TryParse(xPos.Text, out newx))
+                {
+                    currbox.x = newx;
+                    oi.renderBoxes();
+                    spriteBox.Source = BitmapToImageSource(oi.boxbitmap);
+                }
+            }
+        }
+
+        private void yPos_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (boxList.Items.Count > 0)
+            {
+                JonbinBox currbox = (JonbinBox)boxList.Items[boxList.SelectedIndex];
+                float newY;
+                if (float.TryParse(yPos.Text, out newY))
+                {
+                    currbox.y = newY;
+                    oi.renderBoxes();
+                    spriteBox.Source = BitmapToImageSource(oi.boxbitmap);
+                }
+            }
+        }
+
+        private void xScl_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (boxList.Items.Count > 0)
+            {
+                JonbinBox currbox = (JonbinBox)boxList.Items[boxList.SelectedIndex];
+                float newW;
+                if (float.TryParse(xScl.Text, out newW))
+                {
+                    currbox.width = newW;
+                    oi.renderBoxes();
+                    spriteBox.Source = BitmapToImageSource(oi.boxbitmap);
+                }
+            }
+        }
+
+        private void yScl_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (boxList.Items.Count > 0)
+            {
+                JonbinBox currbox = (JonbinBox)boxList.Items[boxList.SelectedIndex];
+                float newH;
+                if (float.TryParse(yScl.Text, out newH))
+                {
+                    currbox.height = newH;
+                    oi.renderBoxes();
+                    spriteBox.Source = BitmapToImageSource(oi.boxbitmap);
+                }
+            }
+        }
+
+        private void SaveItem_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PAC files (*.pac)|*.pac|All files (*.*)|*.*";
+            saveFileDialog.Title = "Save edited PAC file";
+            saveFileDialog.OverwritePrompt = true;
+            saveFileDialog.ShowDialog();
+            if (saveFileDialog.FileName != "")
+            {
+                PACFile.PACEntry colentry = (PACFile.PACEntry)spriteList.Items[spriteList.SelectedIndex];
+                int idx = editedBoxes.BinarySearch(colentry);
+                if (idx < 0) idx = ~idx;
+                editedBoxes.Insert(idx, colentry);
+                byte[] oldpac = File.ReadAllBytes(col.path);
+                foreach (var jb in editedBoxes)
+                {
+                    int writestart = (int)(jb.offset + col.data_start);
+                    int writeend = writestart;
+                    writeend += 4;
+                    writeend += 2 + BitConverter.ToInt16(oldpac, writeend) * 0x20 + 3;
+                    var chunkssize = BitConverter.ToInt32(oldpac, writeend) * 0x50;
+                    writeend += 10 + 41 * 2 + chunkssize;
+                    int boxcount = boxList.Items.Count;
+                    byte[] currfloat = { 0, 0, 0, 0 };
+                    for (var i = 0; i < boxcount; i++)
+                    {
+                        writeend += 4;
+                        var currbox = (JonbinBox)boxList.Items[i];
+                        currfloat = System.BitConverter.GetBytes(currbox.x);
+                        Array.Copy(currfloat, 0, oldpac, writeend, 4);
+                        writeend += 4;
+                        currfloat = System.BitConverter.GetBytes(currbox.y);
+                        Array.Copy(currfloat, 0, oldpac, writeend, 4);
+                        writeend += 4; currfloat = System.BitConverter.GetBytes(currbox.width);
+                        Array.Copy(currfloat, 0, oldpac, writeend, 4);
+                        writeend += 4; currfloat = System.BitConverter.GetBytes(currbox.height);
+                        Array.Copy(currfloat, 0, oldpac, writeend, 4);
+                        writeend += 4;
+                    }
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if(File.Exists(colPath + ".tmp"))
+            {
+                File.Delete(colPath + ".tmp");
+            }
+        }
+
+        private void CloseItem_Click(object sender, RoutedEventArgs e)
+        {
+            spriteList.Items.Clear();
+            boxList.Items.Clear();
+            editedBoxes.Clear();
+        }
+    }
+}
---
+++ StriveHitboxes/OverlaidImage.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace StriveHitboxes
+{
+    class OverlaidImage
+    {
+        int MARGIN = 0;
+        const string JONBIN_HEADER = "JONB";
+        bool is_gg = false;
+        bool is_uni = false;
+        uint height;
+        uint width;
+        uint pxoffsetx;
+        uint pxoffsety;
+        uint texwidth;
+        uint texheight;
+        uint hitboxcount;
+        uint hurtboxcount;
+        public List<JonbinBox> hurtboxes;
+        public List<JonbinBox> hitboxes;
+        public List<JonbinChunk> chunks;
+        Bitmap hipsprite;
+        Bitmap marginbitmap;
+        public Bitmap boxbitmap;
+        Color HURT_COLOR = Color.FromArgb(100, 0, 0, 255);
+        Color HIT_COLOR = Color.FromArgb(100, 255, 0, 0);
+        public OverlaidImage(string colpacpath, int coloffset)
+        {
+            is_gg = true;
+            parseJONB(colpacpath, coloffset);
+            renderBoxes();
+        }
+        private void parseJONB(string colpacpath, int coloffset)
+        {
+            BinaryReader jonbr = new BinaryReader(new FileStream(colpacpath, FileMode.Open));
+            jonbr.BaseStream.Seek((long)coloffset, SeekOrigin.Begin);
+            if (!jonbr.ReadChars(4).SequenceEqual(JONBIN_HEADER.ToCharArray()))
+            {
+                Console.WriteLine("NOT A JONBIN FILE");
+                return;
+            }
+            Console.WriteLine("Image Names: ");
+            short imcount = jonbr.ReadInt16();
+            for (var i = 0; i < imcount; i++)
+            {
+                var strbytes = jonbr.ReadBytes(0x20).TakeWhile(b => (b != 0)).ToArray();
+                Console.WriteLine(Encoding.UTF8.GetString(strbytes, 0, strbytes.Length));
+            }
+            jonbr.BaseStream.Seek(3, SeekOrigin.Current);
+            var chunkcount = jonbr.ReadUInt32();
+            hurtboxcount = (uint)jonbr.ReadInt16();
+            hitboxcount = (uint)jonbr.ReadInt16();
+            chunks = new List<JonbinChunk>();
+            hurtboxes = new List<JonbinBox>();
+            hitboxes = new List<JonbinBox>();
+            jonbr.BaseStream.Seek(41 * 2 + 2, SeekOrigin.Current);
+            if (imcount > 0)
+            {
+                for (var c = 0; c < chunkcount; c++)
+                {
+                    var chunk = new JonbinChunk();
+                    chunk.SrcX = jonbr.ReadSingle();
+                    chunk.SrcY = jonbr.ReadSingle();
+                    chunk.SrcWidth = jonbr.ReadSingle();
+                    chunk.SrcHeight = jonbr.ReadSingle();
+                    chunk.DestX = jonbr.ReadSingle();
+                    chunk.DestY = jonbr.ReadSingle();
+                    chunk.DestWidth = jonbr.ReadSingle();
+                    chunk.DestHeight = jonbr.ReadSingle();
+                    jonbr.BaseStream.Seek(0x20, SeekOrigin.Current);
+                    chunk.Layer = jonbr.ReadInt32();
+                    jonbr.BaseStream.Seek(0xC, SeekOrigin.Current);
+                    chunks.Add(chunk);
+                }
+            }
+            for (var h = 0; h < hurtboxcount; h++)
+            {
+                var hurt = new JonbinBox();
+                hurt.id = jonbr.ReadInt32();
+                hurt.x = jonbr.ReadSingle();
+                hurt.y = jonbr.ReadSingle();
+                hurt.width = jonbr.ReadSingle();
+                if (is_uni)
+                {
+                    hurt.width *= -1;
+                }
+                if (hurt.width < 0 && !is_uni)
+                {
+                    hurt.width *= -1;
+                    is_uni = true;
+                }
+                hurt.height = jonbr.ReadSingle();
+                hurtboxes.Add(hurt);
+            }
+            for (var h = 0; h < hitboxcount; h++)
+            {
+                var hit = new JonbinBox();
+                hit.id = jonbr.ReadInt32();
+                hit.x = jonbr.ReadSingle();
+                hit.y = jonbr.ReadSingle();
+                hit.width = jonbr.ReadSingle();
+                if (is_uni)
+                {
+                    hit.width *= -1;
+                }
+                if (hit.width < 0 && !is_uni)
+                {
+                    hit.width *= -1;
+                    is_uni = true;
+                }
+                hit.height = jonbr.ReadSingle();
+                hitboxes.Add(hit);
+            }
+            jonbr.Close();
+        }
+
+        public void renderBoxes()
+        {
+            if (is_gg)
+            {
+                boxbitmap = new Bitmap(1224, 1224);
+                MARGIN = 0;
+            }
+            else
+            {
+                boxbitmap = new Bitmap((int)texwidth + MARGIN, (int)texheight + MARGIN);
+            }
+            Pen boxpen = new Pen(HURT_COLOR);
+            boxpen.Width = 2;
+            int choffsetx = 640;
+            int choffsety = 802;
+            if (chunks.Count > 0)
+            {
+                choffsetx = -(int)chunks[0].DestX;
+                choffsety = -(int)chunks[0].DestY;
+            }
+            using (Graphics g = Graphics.FromImage(boxbitmap))
+            {
+                if (is_uni)
+                {
+                    /*marginbitmap.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                    g.DrawImage(marginbitmap, pxoffsetx + MARGIN, pxoffsety + MARGIN);
+                    g.ScaleTransform(-1, 1);*/
+                }
+                else if (!is_gg)
+                {
+                    g.DrawImage(marginbitmap, pxoffsetx + MARGIN, pxoffsety + MARGIN);
+                }
+                foreach (var box in hurtboxes)
+                {
+                    float tempx = box.x + choffsetx + MARGIN;
+                    float tempy = box.y + choffsety + MARGIN;
+                    g.DrawRectangle(boxpen, tempx, tempy, box.width, box.height);
+                }
+                boxpen.Color = HIT_COLOR;
+                foreach (var box in hitboxes)
+                {
+                    float tempx = box.x + choffsetx + MARGIN;
+                    float tempy = box.y + choffsety + MARGIN;
+                    g.DrawRectangle(boxpen, tempx, tempy, box.width, box.height);
+                }
+            }
+        }
+    }
+
+    public class JonbinBox
+    {
+        public int id { get; set; }
+        public float x { get; set; }
+        public float y { get; set; }
+        public float width { get; set; }
+        public float height { get; set; }
+        public override string ToString()
+        {
+            return width.ToString() + " by " + height.ToString() + " box at (" + x.ToString() + "," + y.ToString() + ")";
+        }
+    }
+    public class JonbinChunk
+    {
+        public float SrcX { get; set; }
+        public float SrcY { get; set; }
+        public float SrcWidth { get; set; }
+        public float SrcHeight { get; set; }
+        public float DestX { get; set; }
+        public float DestY { get; set; }
+        public float DestWidth { get; set; }
+        public float DestHeight { get; set; }
+        public int Layer { get; set; }
+
+    }
+}
